c46a746f7d7213838f416b5cc6150b825544cf89
okthas <grim.blomkwist@skola.taby.se>
2024-04-19 14:51:59 +0200

    focusing on canvas. Make it easier for me.

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,28 @@
-homeButton = document.getElementsByClassName("headerButtons")[0]
+let homeButton = document.getElementById("homeButton")
+let storeButton = document.getElementById("storeButton")
 
-onclick.homeButton = !1
+let canvas = document.getElementById("pageCanvas")
+const ctx = canvas.getContext("2d")
+
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+console.log(canvas)
+
+
+let homePageImage = new Image()
+homePageImage.src = "images/homePageImage.jpg"
+
+console.log(homePageImage)
+
+homePageImage.onload = function() {
+    let height = canvas.width * homePageImage.height/homePageImage.width
+    ctx.drawImage(homePageImage, 0, 0, homePageImage.width, homePageImage.height, 0, 0, canvas.width, height)
+}
+
+homeButton.addEventListener("click", (e) => {
+    ctx.drawImage(homePageImage, 0, 0, homePageImage.width, homePageImage.height, 0, 0, canvas.width, height)
+})
+
+storeButton.addEventListener("click", (e) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
